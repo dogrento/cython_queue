9daef27d604ada062dc1fe3e669f0dfcd55fce56
dogrento <dogrento93@gmail.com>
2024-01-19 21:15:06 -0300

    covering more tests queue related

--- src/simple_queue.py
+++ src/simple_queue.py
@@ -3,4 +3,11 @@ class Fila():
         self.data = []
 
     def is_empty(self) -> bool:
+        if len(self.data) == 0:
+            return True
+
         return False
+
+    def append_to_fila(self, item):
+        # self.data = item
+        self.data.append(item)
--- tests/test_simple_queue.py
+++ tests/test_simple_queue.py
@@ -11,5 +11,28 @@ def test_is_Fila_empty():
     got = fila.is_empty()
     want = True
 
+    assert got == want    
+
+def test_appending_data_to_Fila():
+    fila = Fila()
+
+    fila.append_to_fila('a')
+    fila.append_to_fila('b')
+    fila.append_to_fila('c')
+
+    got = fila.is_empty()
+    want = False
+
     assert got == want
 
+def test_data_integrity():
+    fila = Fila()
+
+    fila.append_to_fila('a')
+    fila.append_to_fila('b')
+    fila.append_to_fila('c')
+
+    got = fila.data
+    want = ['a', 'b', 'c']
+
+    assert got == want
