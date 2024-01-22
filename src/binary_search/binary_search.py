52652fb093d2c8c51028fe2e3de38d11a00bb5cc
dogrento <dogrento93@gmail.com>
2024-01-22 14:21:03 -0300

    creating test to check data order

--- src/binary_search/binary_search.py
+++ src/binary_search/binary_search.py
@@ -11,4 +11,44 @@ def binarySearch(fila, x, low, high):
 
     return False 
 
+# def sortFila(fila):
+#     if fila.len_fila() <= 1:
+#         return fila
+#     for i in range(fila.len_fila()):
+#         
+#     return [0, 1, 2, 3, 4]
 
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+        
+
+def isOrdered(arr):
+    return True
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,5 +1,5 @@
 from src.simple_queue import Fila, fill_fila
-from src.binary_search.binary_search import binarySearch
+from src.binary_search.binary_search import binarySearch, mergeSort, isOrdered
 
 def test_binary_search():
     fila = Fila()
@@ -10,3 +10,33 @@ def test_binary_search():
     want = True
 
     assert got == want
+
+def test_merge_sort():
+    fila = Fila()
+    fill_fila(fila, 5)
+
+    mergeSort(fila.data)
+    got = fila.data
+    want = [0, 1, 2, 3, 4]
+
+    assert got == want
+
+def test_check_if_ordered():
+    fila = Fila()
+    fill_fila(fila, 5)
+
+    got = isOrdered(fila.data)
+    want = True
+
+    assert got == want
+
+# def test_sort_fila():
+#     fila = Fila()
+#     fill_fila(fila, 5)
+
+#     got = sortFila(fila)
+#     want = [0, 1, 2, 3, 4]
+
+#     assert got == want
+
+    
